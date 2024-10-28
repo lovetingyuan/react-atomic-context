1e46d245df68e6eae8c7ff8be3312bc65549db34
tingyuan <1932294867@qq.com>
2024-10-28 21:33:27 +0800

    chore

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { fixupConfigRules } from '@eslint/compat'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  {
+    ignores: ['**/dist', '**/example'],
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended'
+    )
+  ),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    rules: {
+      'react-hooks/exhaustive-deps': 'error',
+    },
+  },
+]
