b99536a862040b9d819240a6504bb27a1c5be8f3
tingyuan <github@tingyuan.in>
2025-08-08 00:14:09 +0800

    -

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import type {
   ContextsType,
   RootValueType,
 } from './types.ts'
-import React, { useDebugValue, useSyncExternalStore } from 'react'
+import React from 'react'
 import { name } from '../package.json'
 
 const useContext = React.use || React.useContext
@@ -179,54 +179,3 @@ function useAtomicContext<T extends Record<string, unknown>>(context: AtomicCont
 
 export { createAtomicContext, useAtomicContext }
 export type { AtomicContextMethodsType, ContextOnChangeType }
-
-export function createStore<T extends Record<string, unknown>>(
-  initValue: T,
-  options?: {
-    onChange?: ContextOnChangeType<T>
-  }
-) {
-  const keys = Object.keys(initValue) as (keyof T)[]
-  const snapshot = { ...initValue }
-  const methods: AtomicContextMethodsType<T> = Object.create({
-    get(warn = true) {
-      if (warn) {
-        console.warn(
-          `${name}: The "get()" method is only used for development to inspect the current store value.`
-        )
-      }
-      return Object.freeze({ ...snapshot })
-    },
-  })
-  const store: AtomicContextValueType<T> = Object.create(methods)
-  for (const key of keys) {
-    const listeners = new Set<() => void>()
-    const getValue = () => snapshot[key]
-    const subscribe = (cb: () => void) => {
-      listeners.add(cb)
-      return () => {
-        listeners.delete(cb)
-      }
-    }
-    const Key = `${(key as string)[0].toUpperCase()}${(key as string).slice(1)}`
-    // @ts-expect-error good
-    methods[`set${Key}`] = (val: T[keyof T]) => {
-      const oldValue = snapshot[key]
-      snapshot[key] = val
-      options?.onChange?.({ key, oldValue, value: val }, methods)
-      for (const cb of listeners) {
-        cb()
-      }
-    }
-    // @ts-expect-error good
-    methods[`get${Key}`] = () => snapshot[key]
-    Object.defineProperty(store, key, {
-      get: function getStoreValue() {
-        const value = useSyncExternalStore(subscribe, getValue)
-        useDebugValue(value, val => ({ key, value: val }))
-        return value
-      },
-    })
-  }
-  return () => store
-}
